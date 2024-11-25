73e41fdc628e48fb17efed6ae1746c6da44cfab8
root <root@DESKTOP-ATOIEKT.localdomain>
2024-11-25 14:23:55 +0000

    3-CORE-GET-api-topics

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,9 +1,17 @@
 const endpointsJson = require("../endpoints.json");
-const request = require ("supertest")
-const app = require ("../app")
-/* Set up your test imports here */
+const request = require ("supertest");
+const app = require ("../app");
+const seed = require("../db/seeds/seed");
+const db = require("../db/connection");
+const testData = require("../db/data/test-data");
 
-/* Set up your beforeEach & afterAll functions here */
+beforeEach(() => {
+  return seed (testData);
+});
+
+afterAll(() => {
+  return db.end();
+})
 
 describe("GET /api", () => {
   test("200: Responds with an object detailing the documentation for each endpoint", () => {
@@ -14,4 +22,21 @@ describe("GET /api", () => {
         expect(endpoints).toEqual(endpointsJson);
       });
   });
+});
+
+describe("GET /api/topics", () => {
+  test("200: Responds with an array of objects including slug and description", () => {
+    return request(app)
+    .get ("/api/topics")
+    .expect(200)
+    .then(({ body }) => {
+      console.log(body);      
+      body.topics.forEach((topic) => {
+        expect(topic).toMatchObject({
+          slug: expect.any(String),
+          description: expect.any(String),
+        });
+      })
+    })
+  })
 });
--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express');
-const endpointsJson = require("../be-nc-news/endpoints.json");
+const { getApi, getTopics } = require("../be-nc-news/Controllers/api.controller")
+
 
 const app = express();
 
-app.get("/api", (req, res) => {
-    res.status(200).send({ endpoints : endpointsJson});
-});
-   
+app.get("/api", getApi);
+
+app.get("/api/topics", getTopics);
+
+app.use ((err, req, res, next) => {
+    console.log(err);
+    res.status(500).send({ msg: "Error"})
+})
 
 module.exports = app;
