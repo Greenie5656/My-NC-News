6b6a01bea08b91aefecb56f418227828159eea5b
root <root@DESKTOP-ATOIEKT.localdomain>
2024-11-27 12:53:19 +0000

    9-DELETE-comments

--- error-handlers.js
+++ error-handlers.js
@@ -3,7 +3,12 @@ exports.errorHandler = (err, req, res, next) => {
     if (err.status){
         res.status(err.status).send({ msg: err.msg});
     } else if (err.code === "22P02"){
-        res.status(400).send({ msg: "Invalid article id" })
+        // console.log(req.url);
+        if (req.url.startsWith("/api/comments/")) {
+            res.status(400).send({ msg: "Invalid comment id"});
+         } else {
+        res.status(400).send({ msg: "Invalid article id" });
+         }
     } else {
         res.status(500).send({ msg: "Internal Server Error" });
     }
