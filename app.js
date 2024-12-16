002b1dbd3e464b863ba7e85aae64784f39ba8383
root <root@DESKTOP-ATOIEKT.localdomain>
2024-12-16 10:33:40 +0000

    enable CORS for cross-origin requests

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const apiRouter = require("./routes/api.router");
 const { errorHandler } = require("./error-handlers");
 
 
+
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", apiRouter);
